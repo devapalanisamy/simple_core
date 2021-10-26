793eeaa47297b9f1adbc94d41d484846aabfc2e6
Deva Palanisamy <deva.palanisamy@abg.com>
2021-10-26 16:27:11 +0100

    minor update

--- lib/simple_core.dart
+++ lib/simple_core.dart
@@ -1,7 +1,16 @@
 library simple_core;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'package:simple_core/src/navigation/navigation_service.dart';
+export 'package:simple_core/src/navigation/i_navigation_service.dart';
+export 'package:simple_core/src/di_container.dart';
+export 'package:simple_core/src/ui/base_view.dart';
+export 'package:simple_core/src/storage/i_storage_service.dart';
+export 'package:simple_core/src/extensions/http_extension.dart';
+export 'package:simple_core/src/extensions/string_extension.dart';
+export 'package:simple_core/src/localization/localizer.dart';
+export 'package:simple_core/src/navigation/routing_data.dart';
+export 'package:simple_core/src/network/i_rest_client.dart';
+export 'package:simple_core/src/localization/i_localization_service.dart';
+export 'package:simple_core/src/dialog/i_dialog_service.dart';
+export 'package:simple_core/src/dialog/dialog_service.dart';
+export 'package:simple_core/src/connectivity/i_connectivity_service.dart';
